840d79f14b1c69364734f76f41994db9dc0b72ec
zhaolimin <31010524@qq.com>
2018-08-07 09:26:39 +0800

    测试类提交

---
+++ src/test/java/com/test/TestDemo.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import com.pojo.Pet;
+import org.junit.Test;
+
+public class TestDemo {
+
+    @Test
+    public void testOne(){
+        Pet pet=new Pet();
+        pet.show();
+    }
+}
